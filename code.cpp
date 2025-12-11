a4ea28da77f83d28dabd7482e96d3f20b2643758
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-12-11 21:55:39 +0530

    Day-2

--- code.cpp
+++ code.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 
 int main() {
-    cout<<PI;
+    cout<<"****\n"<<"***\n"<<"**\n"<<"*\n";
     return 0;
 }
---
+++ code1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<a<<endl;
+    
+    cout<<sizeof(int);
+
+    return 0;
+}
