0691c31dcae925a1874dc4bd2720fa5af1c01b1d
Vansh Maheshgawli <vmgawli95@gmail.com>
2026-01-09 22:15:25 +0530

    Bit Manipulation

---
+++ BitManipulation/BitwiseOperator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<< (3 & 5)<<endl;
+    cout<< (3 | 5)<<endl;
+    cout<< (3 ^ 5)<<endl;
+    cout<< (~6)<<endl;
+    cout<< (7>>2)<<endl;
+    cout<< (7<<2)<<endl;
+
+
+}
