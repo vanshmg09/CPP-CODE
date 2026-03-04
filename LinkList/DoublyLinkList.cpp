939eb6e694b9cc64f0b687aa2c30b438664f7a1c
Vansh Maheshgawli <vmgawli95@gmail.com>
2026-03-04 21:57:32 +0530

    Linklist

---
+++ LinkList/DoublyLinkList.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* prev;
+
+        Node(int val){
+            data = val;
+            prev = next = NULL;
+        }
+};
+
+class DoublyList{
+    public:
+        Node* head;
+        Node* tail;
+
+        DoublyList(){
+            head = tail = NULL;
+        }
+
+    void push_front(int val){
+        Node* newNode = new Node(val);
+
+        if(head == NULL){
+            head = tail = newNode;
+        }else{
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void printList(){
+        Node* temp = head;
+        
+        while(temp!=NULL){
+            cout<<temp->data<<"<=>";
+            temp = temp->next;
+        }
+        cout<<"NULL\n";
+    }
+
+    void pop_front(){
+        Node* temp = head;
+        if(head==NULL){
+            cout<<"List is empty\n";
+        }else{
+            head = temp->next;
+            head->prev = NULL;
+
+            temp->next = NULL;
+            delete temp;
+        }
+    }
+
+};
+
+int main(){
+    DoublyList dbll;
+    dbll.push_front(4);
+    dbll.push_front(3);
+    dbll.push_front(2);
+    dbll.push_front(1);
+
+    dbll.printList();
+
+    dbll.pop_front();
+    dbll.printList();
+
+
+}
